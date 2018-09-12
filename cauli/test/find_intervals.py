0e67739874d8620f53006102423bd3ad82652443
Buddha Shrestha <shrestbuddha@gmail.com>
2018-09-12 13:57:26 -0500

    find intervals working

--- cauli/test/find_intervals.py
+++ cauli/test/find_intervals.py
@@ -3,8 +3,9 @@ reference : https://codereview.stackexchange.com/questions/178427/given-2-disjoi
 
 '''
 
-
-
+import pandas as pd
+import numpy
+import ast
 def getIntersection(interval_1, interval_2):
     start = max(interval_1[0], interval_2[0])
     end = min(interval_1[1], interval_2[1])
@@ -38,7 +39,16 @@ def return_intersections(intervals1, intervals2):
                 interval1 = next(iter1)
         except StopIteration:
             return
-
-set1 = [(1, 6), (10, 15), (20, 30)]
-set2=[(2, 5), (7, 13),(17,22),(29,35)]
-print(list(return_intersections(set1, set2)))
+#
+# set1 = [(1, 6), (10, 15), (20, 30)]
+# set2=[(2, 5), (7, 13),(17,22),(29,35)]
+# table = pd.read_csv("/home/buddha/thesis/cauli/data/"+str(2) + '/person_segment_naive.csv',sep='\t')
+# x = table.loc[table['person'] == int(40), table.columns != 'person']['segment'].iloc[0]
+# y = table.loc[table['person'] == int(18), table.columns != 'person']['segment'].iloc[0]
+# x = ast.literal_eval(x)
+# x = numpy.array(x, dtype=float)
+# y = ast.literal_eval(y)
+# y = numpy.array(y, dtype=float)
+# print("Y: ",y)
+# print("X: ",x)
+# print(list(return_intersections(x, y)))
